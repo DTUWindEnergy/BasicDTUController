0c6b205bf1cb302b4fe15a0bd69119c623b3f4fb
Emmanuel Branlard <ebra@dtu.dk>
2015-12-10 13:31:52 +0100

    _adding automatically generated def and header files

---
+++ src/dtu_we_controller_bladed/dtu_we_controller_bladed.h
@@ -0,0 +1,3 @@
+//Signatures from Module dtu_we_controller_bladed
+//subroutine DISCON (avrSWAP, aviFAIL, accINFILE, avcOUTNAME, avcMSG) bind(c,name='discon')
+void discon(double* avrSWAP, int* aviFAIL, int* accINFILE, int* avcOUTNAME, int* avcMSG);
